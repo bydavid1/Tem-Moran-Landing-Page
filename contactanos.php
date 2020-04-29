6a4fba1988aabf1a9e2f2c130dc0fab745d3ecf4
byronjd <byronjimenez9911@gmail.com>
2020-04-29 09:57:58 -0600

    Contact form script created

--- contactanos.php
+++ contactanos.php
@@ -3,43 +3,60 @@ include("layouts/header.php");
 ?>
 
  <!-- Contacts-->
- <section class="section-50 section-md-75 section-md-100 section-lg-120 section-xl-150 bg-wild-sand" id="contacts">
-      <div class="shell text-left">
-        <h2><span class="small">Si tienes alguna consulta</span>Contactanos</h2>
-        <form class="rd-mailform text-left" data-form-output="form-output-global" data-form-type="contact" method="post"
-          action="bat/rd-mailform.php">
-          <div class="range offset-top-40 offset-md-top-60">
-            <div class="cell-lg-4 cell-md-6">
-              <div class="form-group postfix-xl-right-40">
-                <input class="form-control" id="contact-name" placeholder="Nombre" type="text" name="name"
-                  data-constraints="@Required">
+ <section id="contact" class="contact-sec">
+  <div class="container">
+    <div class="title">
+      <h2>Contact <span>Us</span></h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pulvinar lacus at feugiat iaculis. Suspendisse at viverra mauris, sit amet facilisis lectus. Pellentesque mattis auctor quam, et feugiat elit volutpat vel.</p>
+    </div>
+    <div class="row">
+      <div class="col-lg-10">
+        <div class="contact-form-wrapper">
+          <form id="contact-form" method="POST" action="sendmail.php">
+            <div class="row">
+              <div class="col-lg-6">
+                <div class="form-group">
+                  <label for="name">Nombre :</label>
+                  <input type="text" class="input-field" name="name" placeholder="Ingrese su nombre completo" required>
+                </div>
               </div>
-              <div class="form-group postfix-xl-right-40">
-                <input class="form-control" id="contact-email" placeholder="Email" type="email" name="email"
-                  data-constraints="@Email @Required">
+              <div class="col-lg-6">
+                <div class="form-group">
+                  <label for="email">Email :</label>
+                  <input type="email" class="input-field" name="email" placeholder="Ingrese su correo" required>
+                </div>
+              </div>
+              <div class="col-lg-6">
+                <div class="form-group">
+                  <label for="subjict">Asunto :</label>
+                  <input type="text" class="input-field" name="subject" placeholder="Ingrese su asunto" required>
+                </div>
+              </div>
+              <div class="col-lg-6">
+                <div class="form-group">
+                  <label for="phone">Telefono :</label>
+                  <input type="text" class="input-field" name="phone" placeholder="Ingrese su numero de telefono">
+                </div>
+              </div>
+              <div class="col-lg-12">
+                <div class="form-group button-area">
+                  <label for="message">Mensaje :</label>
+                  <textarea name="message" class="input-field textarea" placeholder="Ingrese su mensaje" required></textarea>
+                </div>
+              </div>
+              <div class="col-lg-12">
+                <div class="form-group button-area">
+                  <button type="submit" class="btn btn-primary" id="send">Enviar <i class="fas fa-paper-plane"></i></button>
+                </div>
               </div>
             </div>
-            <div class="cell-lg-4 cell-md-6 offset-top-20 offset-md-top-0">
-              <div class="form-group postfix-xl-right-40">
-                <input class="form-control" id="contact-company" placeholder="Compañía" type="text" name="company"
-                  data-constraints="@Required">
-              </div>
-              <div class="form-group postfix-xl-right-40">
-                <input class="form-control" id="contact-subject" placeholder="Asunto" type="text" name="subject"
-                  data-constraints="@Required">
-              </div>
-            </div>
-            <div class="cell-lg-4 offset-top-20 offset-lg-top-0">
-              <div class="form-group postfix-xl-right-40">
-                <textarea class="form-control" id="contact-message" placeholder="Mensaje" name="message"
-                  data-constraints="@Required"></textarea>
-              </div>
-            </div>
-          </div>
-          <button class="btn btn-form btn-default" type="submit">Enviar mensaje</button>
-        </form>
+          </form>
+        </div>
       </div>
-    </section>
+    </div>
+  </div>
+</section>
+<div class="snackbars" id="form-output-global"></div>
 
     <!-- Get In Touch-->
     <section class="section section-75 section-md-100 section-lg-150 section-bottom-40">
@@ -93,3 +110,43 @@ include("layouts/header.php");
 <?php 
 include("layouts/footer.php");
 ?>
+
+<script>
+		$('#contact-form').submit(function () {
+      output = $("#form-output-global");
+		  $("#send").prop('disabled', true);
+		  $("#send").button('loading');
+		  $('#result').html('');
+		  $.ajax({
+		    type: $(this).attr('method'),
+		    url: $(this).attr('action'),
+		    data: $(this).serialize(),
+		    dataType: 'json',
+		    success: function (response) {
+		      if (response.success == true) {
+            output.addClass("active");
+            output.html('<p><span class="fa-check"></span><span>' + response.message + '</span></p>')
+
+            setTimeout(function () {
+											output.removeClass("active");
+										}, 3500);
+
+		        $("#send").button('reset');
+		        $("#send").prop('disabled', false);
+		        $('#covid-form')[0].reset();
+		      } else {
+            output.addClass("active");
+            output.html('<p><span class="fa-warning"></span><span>' + response.message + '</span></p>')
+            setTimeout(function () {
+											output.removeClass("active");
+										}, 3500);
+
+		        $("#send").button('reset');
+		        $("#send").prop('disabled', false);
+		      }
+		    }
+		  })
+
+		  return false;
+		});
+</script>
---
+++ sendmail.php
@@ -0,0 +1,55 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+$valid['success'] = array(
+    'success' => false,
+    'message' => array()
+);
+
+$nombre = $_POST['name'];
+$email = $_POST['email'];
+$asunto = $_POST['subject'];
+$mensaje = $_POST['message'];
+$telefono = $_POST['phone'];
+if ($telefono == "") {
+    $telefono = "No agregado";
+}
+
+
+$to  = 'byronjimenez9911@gmail.com';
+$subjetc = 'tem-moran contact form from: '. $email;
+$messsage = '
+<html>
+<head>
+  <title>Mensaje enviado por '. $nombre .' desde el formulario de contacto de Tem-moran</title>
+</head>
+<body>
+  <h6>Mensaje:</h6>
+  <p>' .$mensaje. '</p>
+  <b>Contacto: </b> '. $telefono .'
+</body>
+</html>
+';
+
+// HTML Message
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+// Aditional headers
+$headers .= 'To: Tem Moran admins<contact@temmoran.com>'. "\r\n";
+
+// Enviarlo
+if(mail($to, $subjetc, $messsage, $headers)){
+    $valid['success'] = true;
+    $valid['message'] = "Mensaje enviado";  
+
+    echo json_encode($valid);
+}else{
+    $valid['success'] = false;
+    $valid['message'] = "Ocurrió un error, intentelo mas tarde";  
+
+    echo json_encode($valid);
+}
+
+?>
